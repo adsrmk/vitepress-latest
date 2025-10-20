8c910f92bf39d1886a72e7090857f25ccac70758
adsrmk <74286623+adsrmk@users.noreply.github.com>
2025-10-20 19:43:51 +0200

    Update config.ts

--- docs/config.ts
+++ docs/config.ts
@@ -17,7 +17,7 @@ export default defineAdditionalConfig({
     },
 
         editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      pattern: 'https://github.com/adsrmk/vitepress-latest/edit/main/docs/:path',
       text: 'Edit this page on GitHub'
     },
    
