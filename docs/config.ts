cb2e4b6a8634e846cb1d5fa415f08384f207222d
adsrmk <74286623+adsrmk@users.noreply.github.com>
2025-10-23 19:56:18 +0200

    Update config.ts

--- docs/config.ts
+++ docs/config.ts
@@ -92,26 +92,24 @@ function sidebarGuide(): DefaultTheme.SidebarItem[] {
     },
 
 
-
-      {
-        text: 'Level 1',
-        items: [
-           { text: 'Users', link: 'users.md' },
-         { text: 'Redirects', link: 'redirect.md' }
-          {
-            text: 'Level 2',
-            items: [
-              {
-                text: 'Level 3',
-                items: [
-                  ...
-                ]
-              }
-            ]
-          }
-        ]
-      },
-  
+{
+  text: 'Level 1',
+  items: [
+    { text: 'Users', link: 'users.md' },
+    { text: 'Redirects', link: 'redirect.md' },
+    {
+      text: 'Level 2',
+      items: [
+        {
+          text: 'Level 3',
+          items: [
+            // more items here
+          ]
+        }
+      ]
+    }
+  ]
+},
     
 
 
