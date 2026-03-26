d23d88a34dc11b04240e0a16ad42fccfb3a5ebc2
adsrmk <74286623+adsrmk@users.noreply.github.com>
2026-03-26 21:59:41 +0100

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,4 +2,23 @@ import Theme from 'vitepress/theme'
 import 'virtual:group-icons.css'
 import './styles.css'
 
+
+
+
+import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import CustomSidebar from './CustomSidebar.vue'
+
+export default {
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'sidebar-nav-before': () => h(CustomSidebar)
+    })
+  }
+}
+
+
+
+
 export default Theme
