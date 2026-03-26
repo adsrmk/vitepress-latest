b8b643d3fd34f6e67666f2b88860f94a1a69ee42
adsrmk <74286623+adsrmk@users.noreply.github.com>
2026-03-26 22:03:33 +0100

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,26 +1,7 @@
 import Theme from 'vitepress/theme'
-import { h } from 'vue'
 import 'virtual:group-icons.css'
 import './styles.css'
 
-export default {
-  ...Theme,
-  enhanceApp({ app }) {
-    if (Theme.enhanceApp) Theme.enhanceApp({ app })
 
-    if (typeof window !== 'undefined') {
-      window.addEventListener('click', (e) => {
-        const clicked = e.target.closest('.VPDocSidebarItem')
 
-        if (!clicked) return
-
-        // close all others
-        document.querySelectorAll('.VPDocSidebarItem.open').forEach(el => {
-          if (el !== clicked) {
-            el.classList.remove('open')
-          }
-        })
-      })
-    }
-  }
-}
+export default Theme
